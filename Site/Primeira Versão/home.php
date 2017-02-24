46c545ff471b6b12b7ba3b7b8c6618780432dc2f
rennan123 <rennan.h.oliveira@gmail.com>
2017-02-23 22:22:03 -0300

    first commit

---
+++ Site/Primeira Versão/home.php
@@ -0,0 +1,81 @@
+<?php
+	
+	session_start();
+
+	if(!isset($_SESSION['usuario'])){
+		header('Location: index.php?erro=1');
+	}
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+
+		<title>Avaliador</title>
+		
+		<!-- jquery - link cdn -->
+		<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+		<!-- bootstrap - link cdn -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	
+	</head>
+
+	<body>
+
+		<!-- Static navbar -->
+		    <nav class="navbar navbar-inverse navbar-static-top">
+		      <div class="container">
+		        <div class="navbar-header">
+		          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+		            <span class="sr-only">Apenas navegação</span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		          </button>
+					
+					<a href="index.php" class="navbar-brand">
+		          		<span class="img-logo">DCC</span>
+					</a>
+				</div>
+	        
+	        <div id="navbar" class="navbar-collapse collapse">
+	          <ul class="nav navbar-nav navbar-right">
+	            <li><a href="sair.php">Sair</a></li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	      </div>
+	    </nav>
+
+
+	    <div class="container">
+	    	
+	    	<br /><br />
+
+	    	<div class="col-md-4"></div>
+	    	<div class="col-md-4">
+	    		Usuário autenticado!!!
+	    		<br />
+	    		<?= $_SESSION['usuario'] ?>
+	    		<br />
+	    		<?= $_SESSION['email'] ?>
+			</div>
+			<div class="col-md-4"></div>
+
+			<div class="clearfix"></div>
+			<br />
+			<div class="col-md-4"></div>
+			<div class="col-md-4"></div>
+			<div class="col-md-4"></div>
+
+		</div>
+
+
+	    </div>
+	
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	
+	</body>
+</html>
